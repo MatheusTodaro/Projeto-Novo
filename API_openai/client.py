712f65de4bb88bc7db92a667ac5b58b45d43b268
MatheusTodaro <matheus.todaro@etec.sp.gov.br>
2025-05-29 00:35:11 -0300

    init4

--- API_openai/client.py
+++ API_openai/client.py
@@ -1,6 +1,5 @@
 import openai
 
-openai.api_key = ""
 
 def get_filme_sinopse(titulo, ano, diretor):
     prompt = f"Crie uma sinopse em apenas 200 caracteres para o filme '{titulo}' do ano {ano}, dirigido por {diretor}"
